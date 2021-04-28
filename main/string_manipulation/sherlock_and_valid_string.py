0a59d84e875553209ee5d01a05bf587f47183b25
hardvester <jeskoed@gmail.com>
2021-04-28 11:45:32 +0200

    c

--- main/string_manipulation/sherlock_and_valid_string.py
+++ main/string_manipulation/sherlock_and_valid_string.py
@@ -6,6 +6,7 @@ if len(string.keys())==1:
 
 elif len(string.values())==2:
     key1,key2=string.keys()
+    print(key1)
     if string[key1]==1 and (key1-1==key2 or key1-1==0):
         print("YES")
     elif string[key2]==1 and (key2-1==key1 or key2-1==0):
